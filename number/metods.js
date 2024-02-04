17fc2963fb8c773bc1dce1cea56fff0fd40c4e5d
partoni <toni_parfenov@mail.ru>
2024-02-04 21:25:16 +0300

    methods number

---
+++ number/metods.js
@@ -0,0 +1,18 @@
+let num =10
+
+console.log(num.toString(2)) // '1010'
+
+Math.floor(3.9) // 3
+Math.ceil(3.1) // 4
+Math.round(3.6) // 4  к ближайшему целому
+Math.random() // от 0 до 0.9
+Math.min(3,4) // 3
+Math.max(3,4,1) //4
+
+let num2 = 13.445
+num2.toFixed(2) //'13.45'
+
+0.2+0.1==0.3  // false   0.30000000000000004
+
+parseInt('10px') // 10
+parseFloat('ads23.3') // NaN
