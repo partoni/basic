f6d349097c984c6fd18dae73924901c8c853b2d1
partoni <toni_parfenov@mail.ru>
2024-02-04 21:02:23 +0300

    other metodes

--- string/metods.js
+++ string/metods.js
@@ -2,4 +2,12 @@ let str = 'My first string'
 
 let slice = str.slice[3,7] //first
 let indexOf = str.indexOf('str') //9
+str= str.replace('first','second') // 'My second string'
+
+let newStr = str.concat('is written', 'by me')//'My second string' is written by me
+let isInclude = str.includes('is') //true
+str.endsWith('me') // true
+str.startsWith('as') // false
+str.substring(5,2) //fir
+str.trim() // remove spaces
 
