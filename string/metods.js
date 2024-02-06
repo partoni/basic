1968e3bc41d5bd8cd5f159530bb72b831a26fa93
partoni <toni_parfenov@mail.ru>
2024-02-06 22:35:32 +0300

    match

--- string/metods.js
+++ string/metods.js
@@ -1,5 +1,5 @@
 let str = 'My first string'
-
+console.log(str.match(/i/g)); //[i,i]
 let slice = str.slice[3,7] //first
 let indexOf = str.indexOf('str') //9
 str= str.replace('first','second') // 'My second string'
@@ -9,5 +9,7 @@ let isInclude = str.includes('is') //true
 str.endsWith('me') // true
 str.startsWith('as') // false
 str.substring(5,2) //fir
-str.trim() // remove spaces
+str.trim() // remove spaces 
+ console.log(str.match(/i/)); //[ 'i', index: 13, input: 'My second string', groups: undefined ]
+ console.log(str.match(/i/g)); //[i]
 
