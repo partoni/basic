570ce0d762b1ab917eb6aa7f9618092ae3166159
partoni <toni_parfenov@mail.ru>
2024-02-24 23:51:29 +0300

    function common

---
+++ function/common.js
@@ -0,0 +1,15 @@
+// 
+function fullName(firstName,lastName = 'Ivanov'){
+return firstName+lastName
+}
+fullName('John') // John Ivanov
+fullName('John' ,'Petrov') // John Petrov
+// Fanction Declaration
+function sayHi() {
+    alert( "Привет" );
+  }
+// Fanction Expression
+let  sayHi = function() {
+    alert( "Привет" );
+  }
+  
